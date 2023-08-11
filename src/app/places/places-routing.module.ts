242e4caa1bc91ad29faf9021ddb6ebcd4fe85597
tokslaw <tokslaw@chiefsoft.com>
2023-08-11 02:38:44 -0400

    added pages

--- src/app/places/places-routing.module.ts
+++ src/app/places/places-routing.module.ts
@@ -5,26 +5,71 @@ import { PlacesPage } from './places.page';
 
 const routes: Routes = [
   {
+    path: 'tabs',
+    component: PlacesPage,
+    children: [
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full',
+      },
+      {
+        path: 'discover',
+        loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
+      },
+      {
+        path: 'new-offer',
+        loadChildren: () => import('./offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+      },
+      {
+        path: 'offer-bookings',
+        loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+      },
+      {
+        path: 'edit/:placeId',
+        loadChildren: () => import('./offers/edit-offers/edit-offers.module').then( m => m.EditOffersPageModule)
+      },
+      {
+        path: ':placeId',
+        loadChildren: () => import('./discover/place-details/place-details.module').then( m => m.PlaceDetailsPageModule)
+      },
+      {
+        path: '',
+        redirectTo: '/places/tabs/discover',
+        pathMatch: 'full',
+      },
+    ]
+  },
+  {
+    path: 'offers',
+    children: [
+      //   {
+      //     path: '',
+      //     loadChildren: () => import('./offers/offers.module').then( m => m.OffersPageModule)
+      // },
+      {
+        path: 'new',
+        loadChildren: () => import('./offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+      },
+      {
+        path: 'edit/:placeId',
+        loadChildren: () => import('./offers/edit-offers/edit-offers.module').then( m => m.EditOffersPageModule)
+      },
+      {
+        path: ':placeId',
+        loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+      },
+
+]
+  },
+  {
     path: '',
-    component: PlacesPage
+    redirectTo: '/places/tabs/discover',
+    pathMatch: 'full',
   },
-  {
-    path: 'discover',
-    loadChildren: () => import('./discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'new-offer',
-    loadChildren: () => import('./offers/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
-  },
-  {
-    path: 'edit-offers',
-    loadChildren: () => import('./offers/edit-offers/edit-offers.module').then( m => m.EditOffersPageModule)
-  },
-  {
-    path: 'offer-bookings',
-    loadChildren: () => import('./offers/offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
-  }
+
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/places/places.module.ts
+++ src/app/places/places.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule} from "@angular/router";
 
 import { IonicModule } from '@ionic/angular';
 
 import { PlacesPageRoutingModule } from './places-routing.module';
 
 import { PlacesPage } from './places.page';
+
+const routes: Routes =[
+  {
+    path: '',
+    component: PlacesPage
+  }
+]
 
 @NgModule({
   imports: [
